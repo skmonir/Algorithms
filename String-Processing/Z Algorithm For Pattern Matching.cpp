87bd2143a3dfa96dda4b792bc3cbc09a48d60972
Sheikh Monir <skmnrcse@gmail.com>
2017-01-17 14:03:43 +0600

    Update Z Algorithm For Pattern Matching.cpp

--- String-Processing/Z Algorithm For Pattern Matching.cpp
+++ String-Processing/Z Algorithm For Pattern Matching.cpp
@@ -12,35 +12,25 @@ int z[N];
 
 int z_algo(string &s, int sz) {
     fill(z, 0);
-    int right = 0;
-    int left = 0;
+    int right = 0, left = 0;
     for (int k = 1; s[k]; k++) {
         if (k > right) {
             left = right = k;
-            while (s[right] && s[right] == s[right - left]) {
-                right++;
-            }
-            z[k] = right - left;
-            right--;
+            while (s[right] && s[right] == s[right - left]) right++;
+            z[k] = right - left, right--;
         } else {
             int kk = k - left;
-            if (k + z[kk] <= right) {
-                z[k] = z[kk];
-            } else {
+            if (k + z[kk] <= right) z[k] = z[kk];    
+            else {
                 left = k;
-                while (s[right] && s[right] == s[right - left]) {
-                    right++;
-                }
-                z[k] = right - left;
-                right--;
+                while (s[right] && s[right] == s[right - left]) right++;
+                z[k] = right - left, right--;
             }
         }
     }
     int res = 0;
     for (int i = 0; s[i]; i++) {
-        if (z[i] == sz) {
-            res++;
-        }
+        if (z[i] == sz) res++;
     }
     return res;
 }
